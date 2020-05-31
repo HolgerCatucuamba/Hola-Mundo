bfb480c16fed3091f8b563053044f89189510292
Alejandro-Almeida <65546754+Alejandro-Almeida@users.noreply.github.com>
2020-05-31 16:34:52 -0500

    Update hola_mundo.cpp

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -4,5 +4,9 @@ using namespace std;
 
 int main(){
     cout<<"Hola Mundo"<<endl;
+    cout<<"Ingrese un mensaje: "<<endl;
+    cin>>mensaje;
+    cout<<"Su mensaje es: "<<mensaje<<endl;
+    system("PAUSE")
     return 0;
 }
